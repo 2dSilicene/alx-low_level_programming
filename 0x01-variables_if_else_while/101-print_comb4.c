55d23ca09ad79b64e3a7469514f3dd04aec07c3b
2dSilicene <99369719+2dSilicene@users.noreply.github.com>
2022-03-14 14:46:17 -0700

    lopiuy

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
+
 /**
- * main - Program entry point
+ * main - entry point
  *
- * Return: 0 on success. Error code otherwise
+ * Description: Ptint all possible different combo of 3 digits
+ *
+ * Return: 0
  */
+
 int main(void)
 {
-        int a, b, c;
-    
-	for (int i = 0; i <= 9; i++)
-	{
-	    for (int j = i + 1; j <= 9; j++)
-	    {
-	        for (int k = j + 1; k <= 9; k++)
-	        {
-	            putchar(i + '0');
-	            putchar(j + '0');
-	            putchar(k + '0');
-	            if (((i * 100) + (j * 10) + k) < 789)
-	            {
-	                putchar(',');
-                    putchar(' ');
+	int i, j, k, l;
 
-	            }
-	        }
-	    }
+	for (i = 0; i < 1000; i++)
+	{
+		j = i / 100; /* hundreds */
+		k = (i / 10) % 10; /* tens */
+		l = i % 10; /* unit */
+
+		if (j < k && k < l)
+		{
+			putchar(j + '0');
+			putchar(k + '0');
+			putchar(l + '0');
+
+			if (i < 700)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
-    return (0);
+	return (0);
 }
